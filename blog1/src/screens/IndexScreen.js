2fa4ff5cb07c6134a8da070c8eddff5cfc44475e
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-01 09:04:06 +0600

    Renamed project folder

---
+++ blog1/src/screens/IndexScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+const IndexScreen = () => {
+	const value = useContext(BlogContext);
+
+	return (
+		<View>
+			<Text>Hello from IndexScreen</Text>
+			<Text>{value}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
