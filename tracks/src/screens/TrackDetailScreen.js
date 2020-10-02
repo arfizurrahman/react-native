8d2c43952c305ed5427fd68a566c7f9f28fe3f3f
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-02 18:49:41 +0600

    Hooked up the navigators

---
+++ tracks/src/screens/TrackDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TrackDetailScreen = () => {
+	return (
+		<View>
+			<Text>Hello from Track Detail Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackDetailScreen;
---
+++ tracks/src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TrackListScreen = () => {
+	return (
+		<View>
+			<Text>Hello from Track List Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
