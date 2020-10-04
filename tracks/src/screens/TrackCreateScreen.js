c5ab3c852a1755548cf54acdf737742e8de599b5
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-04 11:01:15 +0600

    Show map

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
 	return (
-		<View>
-			<Text>Hello from Track Create Screen</Text>
-		</View>
+		<SafeAreaView forceInset={{ top: 'always' }}>
+			<Text h3>Create a Track</Text>
+			<Map />
+		</SafeAreaView>
 	);
 };
 
