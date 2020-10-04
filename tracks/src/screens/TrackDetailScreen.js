affd9b2b8badf5b3107d9b76e67dfb649226f0f8
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-04 23:13:12 +0600

    Showing track details

--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
+import MapView, { Polyline } from 'react-native-maps';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+	const { state } = useContext(TrackContext);
+	const id = navigation.getParam('id');
+
+	const track = state.find((t) => t._id === id);
+
+	if (!track) {
+		return null;
+	}
+
+	const intialCoords = track.locations[0].coords;
+
 	return (
-		<View>
-			<Text>Hello from Track Detail Screen</Text>
-		</View>
+		<Fragment>
+			<Text style={{ fontSize: 18 }}>{track.name}</Text>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					longitudeDelta: 0.01,
+					latitudeDelta: 0.01,
+					...intialCoords,
+				}}>
+				<Polyline coordinates={track.locations.map((loc) => loc.coords)} />
+			</MapView>
+		</Fragment>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	map: {
+		height: 300,
+	},
+});
 
 export default TrackDetailScreen;
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -16,7 +16,10 @@ const TrackListScreen = ({ navigation }) => {
 				keyExtractor={(item) => item._id}
 				renderItem={({ item }) => {
 					return (
-						<TouchableOpacity>
+						<TouchableOpacity
+							onPress={() =>
+								navigation.navigate('TrackDetail', { id: item._id })
+							}>
 							<ListItem bottomDivider>
 								<ListItem.Content>
 									<ListItem.Title>{item.name}</ListItem.Title>
