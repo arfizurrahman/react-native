f7fb9dd9b6baa7e5224e642de4ecc42faf324ddd
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-08 22:33:50 +0600

    Added hooks package

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,13 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	SafeAreaView,
+	Platform,
+	StatusBar,
+	View,
+} from 'react-native';
+import {
+	useDimensions,
+	useDeviceOrientation,
+} from '@react-native-community/hooks';
 
 export default function App() {
+	const { landscape, portrait } = useDeviceOrientation();
+
+	console.log('is orientation portrait: ', portrait);
+	console.log('is orientation landscape: ', landscape);
 	return (
-		<View style={styles.container}>
-			<Text>Hello world!</Text>
-			<StatusBar style='auto' />
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View
+				style={{
+					backgroundColor: 'dodgerblue',
+					width: '100%',
+					height: landscape ? '100%' : '30%',
+				}}></View>
+		</SafeAreaView>
 	);
 }
 
@@ -15,7 +32,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
 	},
 });
