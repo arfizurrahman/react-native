585563671be17369301bc63254852514b764809a
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-09 21:38:11 +0600

    Added list detail and welcome screen, ListItem, Card component

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,37 +1,14 @@
 import React from 'react';
-import {
-	StyleSheet,
-	SafeAreaView,
-	Platform,
-	StatusBar,
-	View,
-} from 'react-native';
-import {
-	useDimensions,
-	useDeviceOrientation,
-} from '@react-native-community/hooks';
+import { View, StyleSheet } from 'react-native';
+
+import AppText from './app/components/AppText';
+import AppButton from './app/components/AppButton';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-	const { landscape, portrait } = useDeviceOrientation();
-
-	console.log('is orientation portrait: ', portrait);
-	console.log('is orientation landscape: ', landscape);
-	return (
-		<SafeAreaView style={styles.container}>
-			<View
-				style={{
-					backgroundColor: 'dodgerblue',
-					width: '100%',
-					height: landscape ? '100%' : '30%',
-				}}></View>
-		</SafeAreaView>
-	);
+	return <ListingDetailsScreen />;
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-	},
-});
+const styles = StyleSheet.create({});
