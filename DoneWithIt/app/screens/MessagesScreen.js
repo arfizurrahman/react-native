5d0e5605fbb4c77a531e305e3639b28d0680cc49
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-09 22:48:20 +0600

    Added Separator Component

--- DoneWithIt/app/screens/MessagesScreen.js
+++ DoneWithIt/app/screens/MessagesScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../components/ListItem';
+import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
 const messages = [
@@ -32,6 +33,7 @@ const MessagesScreen = () => {
 						image={item.image}
 					/>
 				)}
+				ItemSeparatorComponent={ListItemSeparator}
 			/>
 		</Screen>
 	);
