255b5ab6c8896f81180f6526978fb170480a60b9
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-10 03:24:25 +0600

    Created ApptexttInput Component and extracted styles to common file

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+import AppTextInput from './app/components/AppTextInput';
+import Screen from './app/components/Screen';
 
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
-	return <ListingsScreen />;
+	const [firstName, setFirstName] = useState('');
+	return (
+		<Screen>
+			<AppTextInput placeholder='Username' icon='email' />
+		</Screen>
+	);
 }
 
 const styles = StyleSheet.create({});
