fe287a0a841a425eb7619085dec2d75af7e65a7d
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-09 23:20:42 +0600

    Deleting items

--- DoneWithIt/app/components/ListItemDeleteAction.js
+++ DoneWithIt/app/components/ListItemDeleteAction.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableNativeFeedback } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
-const ListItemDeleteAction = () => {
+const ListItemDeleteAction = ({ onPress }) => {
 	return (
-		<View style={styles.container}>
-			<MaterialCommunityIcons name='trash-can' size={35} color={colors.white} />
-		</View>
+		<TouchableNativeFeedback onPress={onPress}>
+			<View style={styles.container}>
+				<MaterialCommunityIcons
+					name='trash-can'
+					size={35}
+					color={colors.white}
+				/>
+			</View>
+		</TouchableNativeFeedback>
 	);
 };
 
