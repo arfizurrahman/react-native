5402cd3047ac7c42477f481bf291de68a2391a7f
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-10 02:53:49 +0600

    Built  the listing screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
-	return <AccountScreen />;
+	return <ListingsScreen />;
 }
 
 const styles = StyleSheet.create({});
