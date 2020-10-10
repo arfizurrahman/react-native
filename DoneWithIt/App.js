db1c05c521b2b900079192dab5971d309ee53f63
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-10 19:18:44 +0600

    Added ImageInputListComponent to ListingEditScreen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,28 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 
-import Screen from './app/components/Screen';
-import ImageInputList from './app/components/ImageInputList';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-	const [imageUris, setImageUris] = useState([]);
-
-	const handleAdd = (uri) => {
-		setImageUris([...imageUris, uri]);
-	};
-	const handleRemove = (uri) => {
-		setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-	};
-
-	return (
-		<Screen>
-			<ImageInputList
-				imageUris={imageUris}
-				onAddImage={handleAdd}
-				onRemoveImage={handleRemove}
-			/>
-		</Screen>
-	);
+	return <ListingEditScreen />;
 }
 
 const styles = StyleSheet.create({});
