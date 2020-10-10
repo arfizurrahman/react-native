cb7cf6bf1f17ab321db621e9dfbb23854ad6c42e
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-10-10 08:53:56 +0600

    Building categoryPickerItem Component

--- DoneWithIt/app/components/AppPicker.js
+++ DoneWithIt/app/components/AppPicker.js
@@ -17,6 +17,7 @@ import PickerItem from './PickerItem';
 const AppPicker = ({
 	icon,
 	items,
+	numberOfColumns = 1,
 	onSelectItem,
 	PickerItemComponent = PickerItem,
 	placeholder,
@@ -56,8 +57,10 @@ const AppPicker = ({
 					<FlatList
 						data={items}
 						keyExtractor={(item) => item.value.toString()}
+						numColumns={numberOfColumns}
 						renderItem={({ item }) => (
 							<PickerItemComponent
+								item={item}
 								label={item.label}
 								onPress={() => {
 									setModalVisible(false);
