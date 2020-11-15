b24df8027da27ab28dddc3cd1ab7bd19a70617ae
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2020-11-15 23:31:53 +0600

    Caching listings

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -11,6 +11,9 @@ import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 
+import NetInfo from '@react-native-community/netinfo';
+import AsyncStorage from '@react-native-community/async-storage';
+
 const Link = () => {
 	const navigation = useNavigation();
 	return (
